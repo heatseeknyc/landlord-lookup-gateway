97b372088cefb5f82997355dae68564ca3f6b605
Ich <wstlabs>
2016-04-19 12:24:43 -0400

    add suppport module for pivoting Geoclient API responses

---
+++ pylib/nycgeo/pivot.py
@@ -0,0 +1,72 @@
+#
+# Utility functions for pivoting (or less elegantly, "scraping") multi-building 
+# record results out of the raw dicts structs returned by the NYC Geoclient API.
+#
+# Basically what happens is that the Geoclient API "vectorizes" its building
+# results not by returning a nested dict struct like one would expect, but by 
+# returning a giant flat dict per-building fields "indexed" by appending raw 
+# integer offsets after the field names (which also happen to start with the
+# prefix 'gi').  So have to do some careful parsing for field names of just this 
+# type (and not other integer-trailing fields, like "censusTrack2000") and then 
+# "pivot" these into a proper nested struct we can actually deal with. 
+#
+import re
+import simplejson as json
+from collections import defaultdict
+
+
+indexpat = re.compile('^(.*?)(\d+)$')
+def splitkey(s):
+    m = indexpat.match(s)
+    if m:
+        return m.group(1),m.group(2)
+    else:
+        return s,None 
+
+def pivot_generic(d):
+    core = {}
+    multi = defaultdict(dict)
+    for k in sorted(d.keys()):
+        base,index = splitkey(k)
+        if index is not None:
+            multi[index][base] = d[k] 
+        else:
+            core[k] = d[k]
+    return core,multi
+
+
+
+def profile_nycgeo_keys(dictlist):
+    count = defaultdict(int) 
+    for d in dictlist:
+        for k in d:
+            count[k] += 1
+    return count
+
+gipat = re.compile('^gi.*?\d+$')
+def is_gi_key(k):
+    return bool(gipat.match(k))
+
+def pivot_nycgeo_partial(d):
+    core = {}
+    multi = defaultdict(dict)
+    for k in d.keys():
+        if is_gi_key(k):
+            base,index = splitkey(k)
+            if index is not None:
+                multi[index][base] = d[k] 
+            else:
+                raise RuntimeError("invalid state")
+        else:
+            core[k] = d[k]
+    return core,multi
+
+
+def pivot_nycgeo(d):
+    taxlot,multi = pivot_nycgeo_partial(d)
+    imulti = {int(k):multi[k] for k in multi.keys()}
+    intkeys = sorted(list(imulti.keys()))
+    buildings = [imulti[k] for k in intkeys]
+    return {'taxlot':taxlot,'buldings':buildings}
+
+
---
+++ tests/test-pivot.pl
@@ -0,0 +1,29 @@
+import sys
+import simplejson as json
+from nycgeo.pivot import pivot_nycgeo, pivot_nycgeo_partial, profile_nycgeo_keys, is_gi_key
+
+
+filename = sys.argv[1]
+d = json.loads(open(filename,"r").read())
+print("dict with %d keys." % len(d))
+
+rawkeys = list(d.keys())
+print("got %d raw keys." % len(rawkeys))
+gikeys = list(filter(is_gi_key,d.keys()))
+print("got %d filtered keys." % len(gikeys))
+core,multi = pivot_nycgeo_partial(d)
+
+print("core with %d keys." % len(core))
+print("multi with %d keys." % len(multi))
+core_keys = sorted(list(core.keys()))
+print("cor keys = ",json.dumps(core_keys,sort_keys=True,indent=True))
+print("multi keys = %s" % sorted(list(multi.keys())))
+
+profile = profile_nycgeo_keys(list(multi.values()))
+print("profile = ",json.dumps(profile,sort_keys=True,indent=True))
+
+print("now...")
+r = pivot_nycgeo(d)
+print(json.dumps(r,sort_keys=True,indent=True))
+
+
