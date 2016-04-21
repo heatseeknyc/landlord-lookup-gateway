43a52f16bb8bbd84723ac1bc151f4811d43b4dcb
Ich <wstlabs>
2016-04-21 10:04:30 -0400

    delete split_address

--- pylib/lookuptool/geoutils.py
+++ pylib/lookuptool/geoutils.py
@@ -2,55 +2,6 @@ import re
 from copy import deepcopy
 
 #
-# Various address-munging functions, mostly-NYC specific, but not otherwise
-# tied to any framework or protocol. 
-#
-
-# 
-# An overly simple address splitter, used in our test suites only.
-# Expects an address of the form:
-#
-#   "43 Mercer Street, Manhattan" 
-#
-# And if parseable, returns a dict of the form:
-#
-#   { "street_name":"Mercer Street", "house_number":"43", "boro_name":"Manhattan" }
-#
-# or None if not parseable. 
-#
-pat = {}
-pat['street_addr'] = re.compile('^(\S+)\s+(.*)$');
-def split_address(rawaddr):
-    terms = split_csv(rawaddr)
-    if len(terms) != 2:
-        return None
-    street_address,boro_name = tuple(terms)
-    t = split_street_address(street_address)
-    if t is None:
-        return None
-    house_number,street_name = t
-    return house_number,street_name,boro_name
-    # return {
-    #    "street_name":street_name,
-    #    "house_number":house_number,
-    #    "boro_name":boro_name
-    # }
-
-# '43 Mercer Street' -> (43,'Mercer Street')
-def split_street_address(street_addr):
-    m = re.match(pat['street_addr'],street_addr)
-    if m:
-        (house_number,street_name) = m.groups()
-        return (house_number,street_name)
-    else:
-        return None
-
-def split_csv(s):
-    return [t.strip() for t in s.split(',')]
-
-
-
-#
 # Municipality => Borough mapping.
 #
 # Whereby we attempt to map the "city" part of an address to one of 
