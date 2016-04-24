47f3c6dd3dcef8d1525a961bcdc46eb54b831c2f
Ich <wstlabs>
2016-04-24 02:42:17 -0400

    client import

--- tests/test-nycgeo.py
+++ tests/test-nycgeo.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 import sys, argparse
 import simplejson as json
-from nycgeo.client.simple import SimpleGeoClient
-
+from nycgeo.client import SimpleGeoClient
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--addr", help="address to parse")
