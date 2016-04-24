9725df4c37f2a9d725d59a20a4a98135de23f822
Ich <wstlabs>
2016-04-24 10:43:55 -0400

    sweet nothingness

--- pylib/lookuptool/__init__.py
+++
@@ -1,8 +0,0 @@
-from .hybrid.factory import instance
-#
-# A convenience method for applications (so they don't have to know 
-# where to find the factory module, above). 
-#
-def get_lookup_agent(**kwargs):
-    return instance(**kwargs)
-
