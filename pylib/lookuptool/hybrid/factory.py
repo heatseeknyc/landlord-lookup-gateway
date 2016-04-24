5cac24f4e08ce4826b8c42b3049b33a916a77b64
Ich <wstlabs>
2016-04-24 10:48:18 -0400

    prune factory class

--- pylib/lookuptool/hybrid/factory.py
+++
@@ -1,9 +0,0 @@
-from nycgeo.client import SimpleGeoClient
-from ..agent import DataClient
-from .lookup import LookupAgent
-
-def instance(dataconf,geoconf):
-    dataclient = DataClient(**dataconf) 
-    geoclient  = SimpleGeoClient(**geoconf)
-    return LookupAgent(dataclient,geoclient)
-
