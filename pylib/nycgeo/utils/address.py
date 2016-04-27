490264dccc637e2657f1ba42556821f48ecd6514
Ich <wstlabs>
2016-04-27 19:59:11 -0400

    modify split_address to consider only first 2 comma-separated terms

--- pylib/nycgeo/utils/address.py
+++ pylib/nycgeo/utils/address.py
@@ -7,28 +7,40 @@ from collections import namedtuple
 #
 NYCGeoAddress = namedtuple('NYCGeoAddress',['houseNumber','street','borough'])
 
-# Some simple address splitting functions.
+
+
+
+
 #
 # Since at present we rely on Google Autocomplete to pre-normalize 
 # addreses for us in the search form, we don't need to make thse
 # functions too flexible for the time being. 
+#
 
 # Expects an address of the form:
 #
 #   "43 Mercer Street, Manhattan" 
 #
-# And if parseable, returns a dict of the form:
+# And if parseable, returns a named tuple of the form: 
 #
-#   { "street_name":"Mercer Street", "house_number":"43", "boro_name":"Manhattan" }
+#   NYCGeoAddress(houseNumber='43',street='Mercer Street',borough='Manhattan')
 #
-# or None if not parseable. 
+# or None if not parseable.  Note that only the first 2 comma-separated terms 
+# are considered; so the raw address 
+#
+#   '285 Lafayette Street, New York, NY, United States' =>
+#
+# will be treated as equivalent to:
+#
+#   '285 Lafayette Street, New York'
+#
 pat = {}
 pat['street_addr'] = re.compile('^(\S+)\s+(.*)$');
 def split_address(rawaddr):
     terms = split_csv(rawaddr)
-    if len(terms) != 2:
+    if len(terms) < 2:
         return None
-    street_address,boro_name = tuple(terms)
+    street_address,boro_name = terms[0:2]
     t = split_street_address(street_address)
     if t is None:
         return None
