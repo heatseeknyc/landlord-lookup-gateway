04bc5bbb7c930ff9296badef6c7359fc25414fde
Ich <wstlabs>
2016-05-02 12:30:56 -0400

    the lovely trivial daemon

---
+++ uwsgi/daemons/trivial.py
@@ -0,0 +1,13 @@
+from flask import Flask  
+app = Flask(__name__) 
+
+@app.route('/foobar')
+def foobar():  
+    return 'Woof!'
+
+# This branch is only needed as a fallback, when running 
+# the script from the shell (for troubleshooting purposes);  
+# it won't be triggered when running under a WSGI. 
+if __name__ == '__main__':  
+    app.run()
+
