831fdddbe66caa3fde8bb9b8aa0a14b4d30dd9d7
Ich <wstlabs>
2016-05-03 20:38:15 -0400

    chmod after push

--- bin/push-nginx.sh
+++ bin/push-nginx.sh
@@ -1,10 +1,15 @@
 #!/bin/sh -ue
 
 DEST=/opt/nginx
+UID=www-data
+GID=www-data
 
 rsync -avz nginx/conf $DEST
 rsync -avz nginx/html $DEST
 rsync -avz nginx/logs $DEST
 rsync -avz nginx/bin $DEST
+
+find $DEST | xargs chown $UID 
+find $DEST | xargs chgrp $GID 
 
 
--- bin/push-uwsgi.sh
+++ bin/push-uwsgi.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -ue
 
 DEST=/opt/uwsgi
+UID=www-data
+GID=www-data
 
 rsync -avz uwsgi/bin $DEST
 rsync -avz uwsgi/config $DEST
@@ -9,4 +11,6 @@ rsync -avz uwsgi/logs $DEST
 rsync -avz uwsgi/pylib $DEST
 rsync -avz uwsgi/tests $DEST
 
+find $DEST | xargs chown $UID 
+find $DEST | xargs chgrp $GID 
 
