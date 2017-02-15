bd811a5400ff833559f0562d254bac29382a3805
wstlabs <notyet@notyet.org>
2017-02-15 17:17:50 -0500

    comment blocks

--- uwsgi/pylib/lookuptool/dataclient/simple.py
+++ uwsgi/pylib/lookuptool/dataclient/simple.py
@@ -1,27 +1,29 @@
-#
-# An unimaginatively named "data client" agent that provides simple accessors 
-# for the record pulls we're likely to need in our UI (along with a few other
-# secret ones for diagnosis).
-#
+"""
+An unimaginatively named "data client" agent that provides simple accessors
+for the record pulls we're likely to need in our UI (along with a few other
+secret ones for diagnosis).
+"""
 from .base import AgentBase
 from common.logging import log
 import simplejson as json
 
 class DataClient(AgentBase):
 
-    # We return the BBL we're selecting on, along with the boro_id, 
-    # along with the response dict, for the convenience of frontend 
-    # handlers (which otherwise would have to pass these along as
-    # separate parameters for what they need to do).
-    #
-    # Special note about the WHERE clause below:  Basically it's saying 
-    # "first try to match on BBL and BIN if both present; otherwise just 
-    # match on BBL with an empty BIN."  Which is precisely our intent:  
-    # the BIN key disambiguates building-specific (HPD+DHCR), so if the BIN 
-    # is present, we want to disambiguate those rows.  If it isn't, then we 
-    # just need the taxbill columns (and the DHCR+HPD columns will be NULL).
-    #
-    # In either case, we're guaranteed to have at most 1 row match in response.
+    """
+    We return the BBL we're selecting on, along with the boro_id,
+    along with the response dict, for the convenience of frontend
+    handlers (which otherwise would have to pass these along as
+    separate parameters for what they need to do).
+
+    Special note about the WHERE clause below:  Basically it's saying
+    "first try to match on BBL and BIN if both present; otherwise just
+    match on BBL with an empty BIN."  Which is precisely our intent:
+    the BIN key disambiguates building-specific (HPD+DHCR), so if the BIN
+    is present, we want to disambiguate those rows.  If it isn't, then we
+    just need the taxbill columns (and the DHCR+HPD columns will be NULL).
+
+    In either case, we're guaranteed to have at most 1 row match in response.
+    """
     def get_summary(self,_bbl,_bin):
         '''Full ownership summary (Taxbill,DHRC,HPD) for a BBL+BIN pair.'''
         log.debug("bbl = %d, bin = %d" % (_bbl,_bin))
@@ -58,14 +60,16 @@ def make_summary(r):
         "dhcr_active": bool(r.get('dhcr_active'))
     }
 
-# Splits the taxbill owner addresss on the embedded '\\n' string 
-# (literally '\'+'\'+'n') from an incoming string, which we take to 
-# be an encoding of "\n"; e.g:
-#
-#   e.g. 'DAKOTA INC. (THE)\\n1 W. 72ND ST.\\nNEW YORK , NY 10023-3486'
-#
-# then strips whitespace from the resulting terms, and returns a nice 
-# list struct.
+"""
+Splits the taxbill owner addresss on the embedded '\\n' string
+(literally '\'+'\'+'n') from an incoming string, which we take to
+be an encoding of "\n"; e.g:
+
+  'DAKOTA INC. (THE)\\n1 W. 72ND ST.\\nNEW YORK , NY 10023-3486'
+
+then strips whitespace from the resulting terms, and returns a nice
+list struct.
+"""
 def expand_address(s):
     if s is None:
         return None
