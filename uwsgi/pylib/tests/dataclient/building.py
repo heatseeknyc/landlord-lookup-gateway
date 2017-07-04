3fb89dc81c64af5b7b7c53cc5d3a785cabef9c7b
wstlabs <notyet@notyet.org>
2017-07-03 22:43:59 -0400

    rename test dir

--- uwsgi/pylib/tests/dataclient/building.py
+++ uwsgi/pylib/tests/dataclient/building.py
@@ -16,7 +16,7 @@ def init_agent(configpath):
 
 def main():
     agent = init_agent("config/postgres.json")
-    with open("tests/data/building.yaml","rtU") as f:
+    with open("tdata/building.yaml","rtU") as f:
         pairs = yaml.load(f)
     print("that be %d test cases." % len(pairs))
 
