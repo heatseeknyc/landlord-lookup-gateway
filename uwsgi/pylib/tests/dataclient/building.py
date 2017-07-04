d88770938d7d74cbe64a5933f227280448bf09da
wstlabs <notyet@notyet.org>
2017-07-04 14:39:18 -0400

    stub get_building accessor + partially passing test case

--- uwsgi/pylib/tests/dataclient/building.py
+++ uwsgi/pylib/tests/dataclient/building.py
@@ -4,6 +4,7 @@ import yaml
 import time, argparse
 import simplejson as json
 from lookuptool.dataclient import DataClient
+from tests.util import compare
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--mode", help="what to pull")
@@ -19,12 +20,25 @@ def main():
     with open("tdata/building.yaml","rtU") as f:
         pairs = yaml.load(f)
     print("that be %d test cases." % len(pairs))
+    dotests(agent,pairs)
+    print("done.")
+
+def evaltest(agent,r):
+    print("::: eval r = %s" % r)
+    print("::: eval query = %s" % r['query'])
+    print("::: eval expected = %s" % r['result'])
+    _bbl = r['query'].get('bbl')
+    _bin = r['query'].get('bin')
+    expected = r['result']
+    bldg_list = agent.get_building(_bbl,_bin)
+    print("building = %s" % bldg_list)
+    status = compare(bldg_list,expected)
+    print("status = %s" % status)
+
+def dotests(agent,pairs):
+    for r in pairs:
+        evaltest(agent,r)
 
 if __name__ == '__main__':
     main()
-
-sys.exit(1)
-"""
-print(json.dumps(r,indent=4,sort_keys=True))
-"""
 
