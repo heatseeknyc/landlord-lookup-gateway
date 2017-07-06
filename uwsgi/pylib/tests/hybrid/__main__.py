846691d7725f6ec0706805c969a076b5b9ff6b0b
wstlabs <notyet@notyet.org>
2017-07-06 18:30:22 -0400

    dynamic file switch

--- uwsgi/pylib/tests/hybrid/__main__.py
+++ uwsgi/pylib/tests/hybrid/__main__.py
@@ -5,24 +5,40 @@ import simplejson as json
 import gateway.hybrid
 from tests.util import compare
 from tests.hybrid.util import initconf
+from tests.decorators import timedsingle
+
+ENDPOINTS = ('lookup','building')
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--mock', dest='mock', action='store_true', help="use the mock service")
     parser.add_argument('--barf', dest='barf', action='store_true', help="barf up config files after reading")
+    parser.add_argument('endpoint', nargs='?', default='all')
     return parser.parse_args()
+
+def datapath(name):
+    if name not in ENDPOINTS:
+        raise ValueError("invalid endpoint")
+    return "tdata/hybrid/%s.yaml"% name
+
+def load_data(name):
+    infile = datapath(name)
+    with open("tdata/hybrid/lookup.yaml","rtU") as f:
+        return yaml.load(f)
+
+def runfor(agent,endpoint):
+    pairs = load_data(endpoint)
+    print("that be %d test cases." % len(pairs))
+    delta,status = dotests(agent,pairs)
+    _status = 'OK' if status else 'BAD'
+    print("status = %s in  %.2f ms" % (_status,delta))
 
 def main():
     args = parse_args()
     dataconf,geoconf = initconf(args)
     agent = gateway.hybrid.instance(dataconf,geoconf)
-    with open("tdata/hybrid/lookup.yaml","rtU") as f:
-        pairs = yaml.load(f)
-    print("that be %d test cases." % len(pairs))
-    t0 = time.time()
-    dotests(agent,pairs)
-    delta = 1000 * (time.time() - t0)
-    print("dt = %.2f millis" % delta)
+    print("tag = %s" % args.endpoint)
+    runfor(agent,args.endpoint)
     print("done.")
 
 def evaltest(agent,spec):
@@ -35,9 +51,11 @@ def evaltest(agent,spec):
     status = compare(response,expected)
     print("status = %s" % status)
 
+@timedsingle
 def dotests(agent,pairs):
     for r in pairs:
         evaltest(agent,r)
+    return True
 
 
 if __name__ == '__main__':
