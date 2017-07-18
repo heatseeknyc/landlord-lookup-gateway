1cc82619f0677aa79ec62618a7bad88dbc22dc75
wstlabs <notyet@notyet.org>
2017-07-18 13:26:10 -0400

    let's call these 'adjust_' rather than 'tweak_' functions

--- uwsgi/pylib/gateway/dataclient/simple.py
+++ uwsgi/pylib/gateway/dataclient/simple.py
@@ -125,21 +125,21 @@ def stagger_taxlot(r):
     rr['condo'] = extract_prefixed(r,'condo',prune=True)
     rr['meta'] = deepcopy(r)
     if rr['acris']:
-        tweak_acris(rr['acris'])
+        adjust_acris(rr['acris'])
     if rr['pluto']:
         inflate_shape(rr['pluto'])
         augment_pluto(rr['pluto'])
-    tweak_condo(rr['condo'])
+    adjust_condo(rr['condo'])
     return rr
 
-def tweak_acris(acris):
+def adjust_acris(acris):
     fixdates(acris)
     amount = acris.get('amount')
     if amount is not None:
         acris['amount'] = round(amount)
 
 # Doesn't do much - basically a placeholder for now
-def tweak_condo(condo):
+def adjust_condo(condo):
     """Tweaks the condo struct, as needed."""
     trim_null(condo,['parent'])
 
