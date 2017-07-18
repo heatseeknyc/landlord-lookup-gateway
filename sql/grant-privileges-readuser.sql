7afbb28af32bb0983525b6f262baed2cb57c4860
wstlabs <notyet@notyet.org>
2017-07-18 10:50:38 -0400

    clarify description

--- sql/grant-privileges-readuser.sql
+++ sql/grant-privileges-readuser.sql
@@ -1,10 +1,13 @@
 --
--- Finally, grant access to our readuser on the hard schema.
+-- Finally, grant access to our readuser on the 'hard' and 'deco' schemas.
 --
 
 begin;
 
 grant usage on schema hard to readuser;
+grant select on all tables in schema hard to readuser;
+
+grant usage on schema deco to readuser;
 grant select on all tables in schema hard to readuser;
 
 commit;
