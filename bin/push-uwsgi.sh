485b6d646c713fb0bd10e8e15dac95be8a228d9a
wstlabs <notyet@notyet.org>
2017-07-19 18:21:43 -0400

    rename target dir

--- bin/push-uwsgi.sh
+++ bin/push-uwsgi.sh
@@ -9,7 +9,7 @@ rsync -avz uwsgi/config $DEST
 rsync -avz uwsgi/daemons $DEST
 rsync -avz uwsgi/logs $DEST
 rsync -avz uwsgi/pylib $DEST
-rsync -avz uwsgi/tests $DEST
+rsync -avz uwsgi/track $DEST
 
 find $DEST | xargs chown $UID 
 find $DEST | xargs chgrp $GID 
